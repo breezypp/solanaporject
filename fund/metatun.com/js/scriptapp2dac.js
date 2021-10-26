a5fa628f9e491463896130f982a28147df357541
frbreezy <suckmyenvy@gmail.com>
2021-10-26 23:09:08 +0200

    -

---
+++ fund/metatun.com/js/scriptapp2dac.js
@@ -0,0 +1,9 @@
+var bzr = angular.module('atun_app', ['timer']);
+
+bzr.controller('page_all_controller', function($scope, $sce, $timeout) {
+    // init
+    $scope.page_all = page_all; //all page info
+
+
+
+})
